5e2ac7172da0d4e23d0112635307adf55476a071
nathan-metko-umn <metko010@umn.edu>
2025-11-19 11:02:40 -0600

    flywheel patch and no more song

--- northstar-robomaster-project/src/robot/standard/standard_control.cpp
+++ northstar-robomaster-project/src/robot/standard/standard_control.cpp
@@ -133,12 +133,12 @@ BuzzerSubsystem buzzerSubsystem(drivers());
 
 PlaySongCommand playTwinkleCommand(&buzzerSubsystem, twinkleTwinkle);
 
-PlaySongCommand playMegalovaniaCommand(&buzzerSubsystem, megalovaniaSong);
+// PlaySongCommand playMegalovaniaCommand(&buzzerSubsystem, megalovaniaSong);
 
-PressCommandMapping ctrlShiftZSong(
-    drivers(),
-    {&playMegalovaniaCommand},
-    RemoteMapState({Remote::Key::CTRL, Remote::Key::SHIFT, Remote::Key::Z}));
+// PressCommandMapping ctrlShiftZSong(
+//     drivers(),
+//     {&playMegalovaniaCommand},
+//     RemoteMapState({Remote::Key::CTRL, Remote::Key::SHIFT, Remote::Key::Z}));
 
 // flywheel subsystem
 FlywheelSubsystem flywheel(
@@ -491,6 +491,11 @@ ToggleCommandMapping bPressedNotCntlPressedBeyblade(
     {&chassisBeyBladeFastCommand},
     RemoteMapState({Remote::Key::B}, {Remote::Key::CTRL}));
 
+ToggleCommandMapping qPressedNormDrive(
+    drivers(),
+    {&chassisDriveCommand},
+    RemoteMapState(RemoteMapState({tap::communication::serial::Remote::Key::Q})));
+
 ToggleCommandMapping rPressedOrientDrive(
     drivers(),
     {&chassisOrientDriveCommand},
@@ -635,12 +640,13 @@ void registerStandardIoMappings(Drivers *drivers)
     drivers->commandMapper.addMap(&ctrlVPressedHopperToggle);
     drivers->commandMapper.addMap(&zPressedNotCtrlWiggle);
     drivers->commandMapper.addMap(&rPressedOrientDrive);
+    drivers->commandMapper.addMap(&qPressedNormDrive);
     drivers->commandMapper.addMap(&crtlShiftEPressedClientDisplay);
     drivers->commandMapper.addMap(&rightSwiitchDownBeyblade);
     drivers->commandMapper.addMap(&leftSwitchDownPressedShoot);
     drivers->commandMapper.addMap(&leftSwitchUpFlywheels);
     drivers->commandMapper.addMap(&rightSwitchUpHopper);
-    drivers->commandMapper.addMap(&ctrlShiftZSong);
+    // drivers->commandMapper.addMap(&ctrlShiftZSong);
 }
 }  // namespace standard_control
 
