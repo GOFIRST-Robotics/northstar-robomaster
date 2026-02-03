20d18897f3271b7cb899401fa6822b32648c4e5c
AllenRoboMasters <voss0266@umn.edu>
2026-02-02 18:26:35 -0600

    Finished refactor testbed

--- northstar-robomaster-project/src/robot/testbed/using_flywheel.hpp
+++ northstar-robomaster-project/src/robot/testbed/using_flywheel.hpp
@@ -10,7 +10,19 @@ using namespace tap::control;
 using namespace src::control::flywheel;
 
 #ifdef USING_FLYWHEEL
-FlywheelSubsystem flywheel(drivers(), LEFT_MOTOR_ID, RIGHT_MOTOR_ID, UP_MOTOR_ID, CAN_BUS);
+FlywheelSubsystem flywheel(
+    drivers(),
+    LEFT_MOTOR_ID,
+    RIGHT_MOTOR_ID,
+    UP_MOTOR_ID,
+    CAN_BUS,
+    tap::motor::RevMotor::PIDConfig{
+        .PIDSlot = 0,
+        .kP = FLYWHEEL_PID_KP,
+        .kI = FLYWHEEL_PID_KI,
+        .kD = FLYWHEEL_PID_KD,
+        .kF = FLYWHEEL_PID_KF,
+    });
 
 // flywheel commands
 FlywheelRunCommand flywheelRunCommand(&flywheel);
--- northstar-robomaster-project/src/robot/testbed/using_turret.hpp
+++ northstar-robomaster-project/src/robot/testbed/using_turret.hpp
@@ -42,23 +42,26 @@ StandardTurretSubsystem turretSubsystem(
     &getTurretMCBCanComm());
 
 // turret controlers
-algorithms::ChassisFramePitchTurretController chassisFramePitchTurretController(
-    turretSubsystem.pitchMotor,
-    chassis_rel::PITCH_PID_CONFIG);
+src::control::turret::algorithms::
+    ChassisFramePitchTurretController chassisFramePitchTurretController(
+        turretSubsystem.pitchMotor,
+        chassis_rel::PITCH_PID_CONFIG);
 
-algorithms::ChassisFrameYawTurretController chassisFrameYawTurretController(
+src::control::turret::algorithms::ChassisFrameYawTurretController chassisFrameYawTurretController(
     turretSubsystem.yawMotor,
     chassis_rel::YAW_PID_CONFIG);
 
-algorithms::WorldFrameYawChassisImuTurretController worldFrameYawChassisImuController(
-    *drivers(),
-    turretSubsystem.yawMotor,
-    world_rel_chassis_imu::YAW_PID_CONFIG);
+src::control::turret::algorithms::
+    WorldFrameYawChassisImuTurretController worldFrameYawChassisImuController(
+        *drivers(),
+        turretSubsystem.yawMotor,
+        world_rel_chassis_imu::YAW_PID_CONFIG);
 
-algorithms::WorldFramePitchChassisImuTurretController worldFramePitchChassisImuController(
-    *drivers(),
-    turretSubsystem.pitchMotor,
-    world_rel_chassis_imu::PITCH_PID_CONFIG);
+src::control::turret::algorithms::
+    WorldFramePitchChassisImuTurretController worldFramePitchChassisImuController(
+        *drivers(),
+        turretSubsystem.pitchMotor,
+        world_rel_chassis_imu::PITCH_PID_CONFIG);
 
 tap::algorithms::SmoothPid worldFramePitchTurretPosPid(world_rel_turret_imu::PITCH_POS_PID_CONFIG);
 
@@ -69,31 +72,34 @@ tap::algorithms::SmoothPid worldFrameYawTurretPosPid(world_rel_turret_imu::YAW_P
 tap::algorithms::SmoothPid worldFrameYawTurretVelPid(world_rel_turret_imu::YAW_VEL_PID_CONFIG);
 
 // for imu can com giving imu data from turret to chassis
-algorithms::
+src::control::turret::algorithms::
     WorldFramePitchTurretCanImuCascadePidTurretController worldFramePitchTurretCanImuController(
         getTurretMCBCanComm(),
         turretSubsystem.pitchMotor,
         worldFramePitchTurretPosPid,
         worldFramePitchTurretVelPid);
 
-algorithms::WorldFrameYawTurretCanImuCascadePidTurretController worldFrameYawTurretCanImuController(
-    getTurretMCBCanComm(),
-    turretSubsystem.yawMotor,
-    worldFrameYawTurretPosPid,
-    worldFrameYawTurretVelPid);
+src::control::turret::algorithms::
+    WorldFrameYawTurretCanImuCascadePidTurretController worldFrameYawTurretCanImuController(
+        getTurretMCBCanComm(),
+        turretSubsystem.yawMotor,
+        worldFrameYawTurretPosPid,
+        worldFrameYawTurretVelPid);
 
 // for imu fixed on turret
-algorithms::WorldFramePitchTurretImuCascadePidTurretController worldFramePitchTurretImuController(
-    *drivers(),
-    turretSubsystem.pitchMotor,
-    worldFramePitchTurretPosPid,
-    worldFramePitchTurretVelPid);
+src::control::turret::algorithms::
+    WorldFramePitchTurretImuCascadePidTurretController worldFramePitchTurretImuController(
+        *drivers(),
+        turretSubsystem.pitchMotor,
+        worldFramePitchTurretPosPid,
+        worldFramePitchTurretVelPid);
 
-algorithms::WorldFrameYawTurretImuCascadePidTurretController worldFrameYawTurretImuController(
-    *drivers(),
-    turretSubsystem.yawMotor,
-    worldFrameYawTurretPosPid,
-    worldFrameYawTurretVelPid);
+src::control::turret::algorithms::
+    WorldFrameYawTurretImuCascadePidTurretController worldFrameYawTurretImuController(
+        *drivers(),
+        turretSubsystem.yawMotor,
+        worldFrameYawTurretPosPid,
+        worldFrameYawTurretVelPid);
 
 // turret commands
 user::TurretUserControlCommand turretUserControlCommand(
