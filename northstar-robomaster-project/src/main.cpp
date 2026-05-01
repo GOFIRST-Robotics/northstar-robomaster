d6535a0ce57d40aa8a3a22995dd47f6666ffb3ab
AllenRoboMasters <voss0266@umn.edu>
2026-04-30 20:35:35 -0500

    Change to startup in main, 0.001 kp for imu

--- northstar-robomaster-project/src/main.cpp
+++ northstar-robomaster-project/src/main.cpp
@@ -179,7 +179,7 @@ static void initializeIo(Drivers *drivers)
 
     drivers->refSerial.initialize();
 
-    drivers->bmi088.initialize(500, 0.0f, 0.000f);
+    drivers->bmi088.initialize(500, 0.001f, 0.000f);
     drivers->bmi088.setTargetTemperature(35.0f);
     drivers->bmi088.setCalibrationSamples(4000);
 
