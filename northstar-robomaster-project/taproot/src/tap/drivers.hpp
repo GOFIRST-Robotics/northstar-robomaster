44ab4a52d8958d29a2bebdaa2259ffffe33a5d1b
nathan-metko-umn <metko010@umn.edu>
2026-05-02 15:22:01 -0500

    cooked?

--- northstar-robomaster-project/taproot/src/tap/drivers.hpp
+++ northstar-robomaster-project/taproot/src/tap/drivers.hpp
@@ -25,49 +25,31 @@
 #define TAPROOT_DRIVERS_HPP_
 
 #if defined(PLATFORM_HOSTED) && defined(ENV_UNIT_TESTS)
-#include "tap/architecture/profiler.hpp"
-#include "tap/mock/analog_mock.hpp"
 #include "tap/mock/bmi088_mock.hpp"
 #include "tap/mock/can_mock.hpp"
 #include "tap/mock/can_rx_handler_mock.hpp"
 #include "tap/mock/command_mapper_mock.hpp"
-#include "tap/mock/command_scheduler_mock.hpp"
 #include "tap/mock/digital_mock.hpp"
-#include "tap/mock/dji_motor_terminal_serial_handler_mock.hpp"
 #include "tap/mock/dji_motor_tx_handler_mock.hpp"
-#include "tap/mock/error_controller_mock.hpp"
 #include "tap/mock/leds_mock.hpp"
 #include "tap/mock/pwm_mock.hpp"
 #include "tap/mock/ref_serial_mock.hpp"
 #include "tap/mock/remote_mock.hpp"
-#include "tap/mock/scheduler_terminal_handler_mock.hpp"
-#include "tap/mock/terminal_serial_mock.hpp"
 #include "tap/mock/uart_mock.hpp"
-
-#include ""
-
+#include "tap/mock/command_scheduler_mock.hpp"
 #else
-#include "tap/architecture/profiler.hpp"
 #include "tap/communication/can/can.hpp"
 #include "tap/communication/can/can_rx_handler.hpp"
-#include "tap/communication/gpio/analog.hpp"
 #include "tap/communication/gpio/digital.hpp"
 #include "tap/communication/gpio/leds.hpp"
 #include "tap/communication/gpio/pwm.hpp"
 #include "tap/communication/sensors/imu/bmi088/bmi088.hpp"
 #include "tap/communication/serial/ref_serial.hpp"
 #include "tap/communication/serial/remote.hpp"
-#include "tap/communication/serial/terminal_serial.hpp"
 #include "tap/communication/serial/uart.hpp"
 #include "tap/control/command_mapper.hpp"
-#include "tap/control/command_scheduler.hpp"
-#include "tap/control/scheduler_terminal_handler.hpp"
-#include "tap/errors/error_controller.hpp"
-#include "tap/motor/dji_motor_terminal_serial_handler.hpp"
 #include "tap/motor/dji_motor_tx_handler.hpp"
-
-#include "communication/serial/fly_sky.hpp"
-
+#include "tap/control/command_scheduler.hpp"
 #endif
 
 namespace tap
@@ -82,9 +64,7 @@ public:
 protected:
 #endif
     Drivers()
-        : profiler(this),
-          analog(),
-          can(),
+        : can(),
           canRxHandler(this),
           digital(),
           leds(),
@@ -92,25 +72,17 @@ protected:
           refSerial(this),
           remote(this),
           uart(),
-          terminalSerial(this),
           commandMapper(this),
-          //   schedulerTerminalHandler(this),
-          errorController(this),
-          //   djiMotorTerminalSerialHandler(this),
           djiMotorTxHandler(this),
-          //   revMotorTxHandler(this),
           bmi088(this),
 #ifdef ENV_UNIT_TESTS
           commandScheduler(this)
 #else
           commandScheduler(this, true)
 #endif
-    {
-    }
+          {}
 
 #if defined(PLATFORM_HOSTED) && defined(ENV_UNIT_TESTS)
-    arch::Profiler profiler;
-    testing::NiceMock<mock::AnalogMock> analog;
     testing::NiceMock<mock::CanMock> can;
     testing::NiceMock<mock::CanRxHandlerMock> canRxHandler;
     testing::NiceMock<mock::DigitalMock> digital;
@@ -119,44 +91,26 @@ protected:
     testing::NiceMock<mock::RefSerialMock> refSerial;
     testing::NiceMock<mock::RemoteMock> remote;
     testing::NiceMock<mock::UartMock> uart;
-    testing::NiceMock<mock::TerminalSerialMock> terminalSerial;
     testing::NiceMock<mock::CommandMapperMock> commandMapper;
-    // testing::NiceMock<mock::SchedulerTerminalHandlerMock> schedulerTerminalHandler;
-    testing::StrictMock<mock::ErrorControllerMock> errorController;
-    // testing::NiceMock<mock::DjiMotorTerminalSerialHandlerMock> djiMotorTerminalSerialHandler;
     testing::NiceMock<mock::DjiMotorTxHandlerMock> djiMotorTxHandler;
-    // revMotorTxHandler;
     testing::NiceMock<mock::Bmi088Mock> bmi088;
     testing::NiceMock<mock::CommandSchedulerMock> commandScheduler;
 #else
 public:
-    arch::Profiler profiler;
-    gpio::Analog analog;
     can::Can can;
     can::CanRxHandler canRxHandler;
     gpio::Digital digital;
     gpio::Leds leds;
     gpio::Pwm pwm;
     communication::serial::RefSerial refSerial;
-//#define FLY_SKY
-#ifdef FLY_SKY
-    communication::serial::FlySky remote;
-#else
     communication::serial::Remote remote;
-#endif
     communication::serial::Uart uart;
-    communication::serial::TerminalSerial terminalSerial;
     control::CommandMapper commandMapper;
-    // control::SchedulerTerminalHandler schedulerTerminalHandler;
-    errors::ErrorController errorController;
-    // motor::DjiMotorTerminalSerialHandler djiMotorTerminalSerialHandler;
     motor::DjiMotorTxHandler djiMotorTxHandler;
-    // motor::RevMotorTxHandler revMotorTxHandler;
     communication::sensors::imu::bmi088::Bmi088 bmi088;
     control::CommandScheduler commandScheduler;
 #endif
-    static constexpr uint8_t DT = 1000 / 500;  // ms, main loop period
-};                                             // class Drivers
+};  // class Drivers
 
 }  // namespace tap
 
