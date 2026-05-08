8906eb9a3d59921355108261d512d07a044ab729
AllenRoboMasters <voss0266@umn.edu>
2026-05-07 19:17:18 -0500

    buffer for certain range

--- northstar-robomaster-project/src/control/turret/algorithms/chassis_frame_imu_cal_turret_controller.cpp
+++ northstar-robomaster-project/src/control/turret/algorithms/chassis_frame_imu_cal_turret_controller.cpp
@@ -36,11 +36,14 @@ ChassisFrameYawImuCalTurretController::ChassisFrameYawImuCalTurretController(
     TurretMotor &yawMotor,
     const tap::algorithms::SmoothPidConfig &pidConfig,
     float errorForMaxOuput,
-    float maxOutput)
+    float maxOutput,
+    float errorForAveraging)
     : TurretYawControllerInterface(yawMotor),
       pid(pidConfig),
       errorForMaxOuput(errorForMaxOuput),
-      maxOutput(maxOutput)
+      maxOutput(maxOutput),
+      errorForAveraging(errorForAveraging),
+      positionBuffer(std::deque<float>())
 {
 }
 
@@ -67,6 +70,15 @@ void ChassisFrameYawImuCalTurretController::runController(
     if (positionControllerError > errorForMaxOuput)
     {
         turretMotor.setMotorOutput(limitVal<float>(pidOutput, -maxOutput, maxOutput));
+    }
+    else if (positionControllerError < errorForAveraging)
+    {
+        if (positionBuffer.size() > 10)
+        {
+            positionBuffer.pop_front();
+        }
+        positionBuffer.push_back(positionControllerError);
+        turretMotor.setMotorOutput(getPositionBufferAverage());
     }
     else
     {
@@ -99,11 +111,14 @@ ChassisFramePitchImuCalTurretController::ChassisFramePitchImuCalTurretController
     TurretMotor &pitchMotor,
     const tap::algorithms::SmoothPidConfig &pidConfig,
     float errorForMaxOuput,
-    float maxOutput)
+    float maxOutput,
+    float errorForAveraging)
     : TurretPitchControllerInterface(pitchMotor),
       pid(pidConfig),
       errorForMaxOuput(errorForMaxOuput),
-      maxOutput(maxOutput)
+      maxOutput(maxOutput),
+      errorForAveraging(errorForAveraging),
+      positionBuffer(std::deque<float>())
 {
 }
 
@@ -138,6 +153,15 @@ void ChassisFramePitchImuCalTurretController::runController(
     if (positionControllerError > errorForMaxOuput)
     {
         turretMotor.setMotorOutput(limitVal<float>(pidOutput, -maxOutput, maxOutput));
+    }
+    else if (positionControllerError < errorForAveraging)
+    {
+        if (positionBuffer.size() > 10)
+        {
+            positionBuffer.pop_front();
+        }
+        positionBuffer.push_back(positionControllerError);
+        turretMotor.setMotorOutput(getPositionBufferAverage());
     }
     else
     {
